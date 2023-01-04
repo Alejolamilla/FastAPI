acb9f51772a5bb30e1a16c4581ed4ccf4f460ed8
Alphalejo <keatola16.ktl@gmail.com>
2023-01-03 21:56:27 -0500

    Query parameters, optional and mandatory

--- hello_world.py
+++ hello_world.py
@@ -5,7 +5,7 @@ from typing import Optional
 from pydantic import BaseModel
 
 # FastAPI
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Query
 
 app = FastAPI()
 
@@ -24,5 +24,14 @@ def home():
 # Request and response body
 
 @app.post("/person/new")
-def create_person(person: Person = Body(...)):
+def create_person(person: Person = Body(...)): #variale persion = class Person = object body will response with a body (json) the "..." means that it's mandatory to recive the parameters
     return person
+
+# Validations: Query Parameters
+
+@app.get("/person/detail")
+def show_person(
+        name: Optional[str] = Query(None, min_length=1, max_length=50), # name is an optional string = Query(default_value, min, max)
+        age: str = Query(...) #Query parameters shouldn't be mandatory, age is an string = Query (mandatory input)
+    ):
+    return {name : age}
